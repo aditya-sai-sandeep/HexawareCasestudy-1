4101445171ef531ca09c003b0aadd7353c98e045
aditya nagavolu <adityanagavolu@adityas-MacBook-Air.local>
2023-12-18 13:11:51 +0530

    first commit

---
+++ util/PropertyUtil.py
@@ -0,0 +1,8 @@
+class PropertyUtil:
+
+    def getPropertyString():
+        host = 'localhost'
+        username = 'root'
+        password = 'Mysql@2023'
+        database = 'casestudy'
+        return host, username, password, database
